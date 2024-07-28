069fbb970f9628bdf8df76ceda22014602820ee7
정섭 <tlswjdtjq00@skuniv.ac.kr>
2024-07-28 16:44:37 +0900

    Add files via upload

--- lib/view/shin.dart
+++ lib/view/shin.dart
@@ -1,18 +1,125 @@
 import 'package:flutter/material.dart';
 
 class Shin extends StatefulWidget {
-  const Shin({super.key});
+  const Shin ({super.key});
 
   @override
   State<Shin> createState() => _ShinState();
 }
-
-class _ShinState extends State<Shin> {
+  class _ShinState extends State<Shin>{
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('신정섭'),
+        backgroundColor: Colors.yellow,
+        title: const Text("피카츄",
+        style: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+        ),
+        ),
+      ),
+      body: const Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: CircleAvatar(
+                backgroundImage: AssetImage("images/pikachu.jpg"),
+                radius: 100,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(13),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.account_circle_outlined,
+                    size: 40,
+                  ),
+                  Text(
+                    "이름 : 피카츄",
+                    style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.all(13),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.male,
+                    size: 40,
+                  ),
+                  Text(
+                    "성별 : 수컷",
+                    style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.all(13),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.face,
+                    size: 40,
+                  ),
+                  Text(
+                    "트레이너 : 한지우",
+                    style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.all(13),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.bolt,
+                    size: 40,
+                  ),
+                  Text(
+                    "타입 : 전기",
+                    style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.all(13),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.menu_book,
+                    size: 40,
+                  ),
+                  Text(
+                    "기술 : 10만 볼트",
+                    style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            
+          ],
+        ),
       ),
     );
   }
-}  
+}//End
+
+//이름 나이 성별 
